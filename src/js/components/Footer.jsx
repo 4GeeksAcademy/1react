768890c16c82a9b978daaa1ce5464322819a9053
Alex Guillen <ag99u24@gmail.com>
2026-04-20 10:14:42 +0000

    hola

---
+++ src/js/components/Footer.jsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+
+    return (
+            <footer className="bg-dark text-white text-center py-4">
+      <div className="container">
+        <p className="mb-1">© 2026 Tu Proyecto</p>
+        <p className="mb-0">
+          Hecho por mi con ❤️ usando React y Bootstrap
+        </p>
+      </div>
+    </footer>
+    )
+}
+
+export default Footer
